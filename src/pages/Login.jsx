2cbd4a85f15b79b43855cfbea0723f7e2feb5428
yksl <yukselkurtuldu9@gmail.com>
2024-04-30 23:12:05 +0300

    edit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
   return (
     <div className="flex justify-center">
       <div className="overflow-hidden flex-1 h-screen justify-center items-center dark:bg-gray-dark-main">
-        <div className={`form-container mt-[5vh] w-[380px] h-[580px] `}>
+        <div className={`form-container mt-[5vh] w-[380px] h-[30rem] `}>
           <form onSubmit={handleSubmit}>
             <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
               Sign In
