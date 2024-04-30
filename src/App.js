f578503336db70dd7c3322c6160f9462af75d782
yksl <yukselkurtuldu9@gmail.com>
2024-04-30 21:52:21 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import AppRouter from "./router/AppRouter";
 import AuthProvider from "./context/AuthProvider";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <AuthProvider>
       <Navbar/>
       <AppRouter />
+      <ToastContainer />
     </AuthProvider>
   );
 };
