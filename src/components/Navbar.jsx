692fbb5fcef06c98c913efebe202c0c73c3a2f68
yksl <yukselkurtuldu9@gmail.com>
2024-05-02 23:24:00 +0300

    active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
               </a>
             </div>
             <div>
-              <NavLink className="mr-8" to={"/"}>Movies</NavLink>
+              <NavLink className="mr-5" to={"/"}>Movies</NavLink>
               <NavLink to={"/series"}>Series</NavLink>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
