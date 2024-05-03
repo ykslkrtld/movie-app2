9c0035041e32dd112d2b9c90bf3a08a842c839f7
yksl <yukselkurtuldu9@gmail.com>
2024-05-03 14:05:47 +0300

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,17 +51,17 @@ export default function Navbar() {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
+                        to={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white' : 'text-black-500 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
@@ -143,18 +143,18 @@ export default function Navbar() {
         <Disclosure.Panel className="sm:hidden">
         <div className="space-y-1 px-2 pb-3 pt-2">
           {navigation.map((item) => (
-            <Disclosure.Button
+            <NavLink
               key={item.name}
-              as="a"
-              href={item.href}
+              // as="a"
+              to={item.href}
               className={classNames(
-                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                item.current ? 'bg-gray-900 text-white' : 'text-black-500 hover:bg-gray-700 hover:text-white',
                 'block rounded-md px-3 py-2 text-base font-medium'
               )}
               aria-current={item.current ? 'page' : undefined}
             >
               {item.name}
-            </Disclosure.Button>
+            </NavLink>
           ))}
         </div>
       </Disclosure.Panel>
