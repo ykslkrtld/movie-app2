b5a70f0733acbd9487613c085959f1c22eb3c397
yksl <yukselkurtuldu9@gmail.com>
2024-05-03 10:11:58 +0300

    menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,17 @@ import Switch from "./Switch";
 import { useAuthContext } from "../context/AuthProvider";
 import avatar from "../assets/icons/avatar.png";
 import { NavLink, Link } from "react-router-dom";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Movies', href: '/', current: false },
+  { name: 'Series', href: '/series', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 
 export default function Navbar() {
   const { currentUser, logOut } = useAuthContext();
@@ -13,17 +24,52 @@ export default function Navbar() {
         as="nav"
         className="bg-neutral-100 dark:bg-gray-900 py-3 dark:text-white fixed w-full top-0 z-20"
       >
+        {({ open }) => (
+          <>
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
-            <div className="flex flex-shrink-0 items-center">
+            {/* <div className="flex flex-shrink-0 items-center">
               <a className="dark:text-white text-dark" href="/">
                 Movies
               </a>
-            </div>
-            <div>
+            </div> */}
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            {/* <div>
               <NavLink className="mr-5" to={"/"}>Movies</NavLink>
               <NavLink to={"/series"}>Series</NavLink>
-            </div>
+            </div> */}
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <p className="capitalize me-2 dark:text-white">
                 {currentUser?.displayName}
@@ -57,7 +103,7 @@ export default function Navbar() {
                           {({ active }) => (
                             <Link
                               to={"/register"}
-                              className="active:bg-gray-100                               block px-4 py-2 text-sm text-gray-700"
+                              className="active:bg-gray-100 block px-4 py-2 text-sm text-gray-700"
                             >
                               Register
                             </Link>
@@ -94,6 +140,26 @@ export default function Navbar() {
             </div>
           </div>
         </div>
+        <Disclosure.Panel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {navigation.map((item) => (
+            <Disclosure.Button
+              key={item.name}
+              as="a"
+              href={item.href}
+              className={classNames(
+                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium'
+              )}
+              aria-current={item.current ? 'page' : undefined}
+            >
+              {item.name}
+            </Disclosure.Button>
+          ))}
+        </div>
+      </Disclosure.Panel>
+        </>
+         )}
       </Disclosure>
       <div className="h-[3.5rem] dark:bg-gray-dark-main mt-10"></div>
     </>
